9f9a650847882f88f813ca96b6cb17d5a4b4de97
claudym <claudy.m.mejia@gmail.com>
2023-05-19 02:25:25 -0400

    Refactorizar Frontend

--- Frontend/src/utils/requests.js
+++ Frontend/src/utils/requests.js
@@ -8,7 +8,7 @@ const request = axios.create({
 
 export const fetchContribuyentes = async () => {
   try {
-    const response = await request.get("/contribuyentes");
+    const response = await request.get("/Contribuyente");
     return response.data.data;
   } catch (error) {
     throw new Error(error.response.data.message);
@@ -17,7 +17,7 @@ export const fetchContribuyentes = async () => {
 
 export const fetchComprobantes = async () => {
   try {
-    const response = await request.get("/comprobantesfiscales");
+    const response = await request.get("/ComprobanteFiscal");
     return response.data.data;
   } catch (error) {
     throw new Error(error.response.data.message);
@@ -26,7 +26,7 @@ export const fetchComprobantes = async () => {
 
 export const fetchTotalITBISList = async () => {
   try {
-    const response = await request.get("/ComprobantesFiscales/ITBIS/Total");
+    const response = await request.get("/ComprobanteFiscal/ITBIS/Total");
     return response.data.data;
   } catch (error) {
     throw new Error(error.response.data.message);
@@ -36,7 +36,7 @@ export const fetchTotalITBISList = async () => {
 export const fetchTotalITBISDetalle = async (rncCedula) => {
   try {
     const response = await request.get(
-      `/comprobantesfiscales/${rncCedula}/ITBIS/Total`
+      `/ComprobanteFiscal/${rncCedula}/ITBIS/Total`
     );
     return response.data.data;
   } catch (error) {
